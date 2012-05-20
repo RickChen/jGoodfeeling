995899b1c815ecef75a666ec7336d8e1daac559b
Rick Chen <rick@rickchendesign.com>
2012-05-19 21:06:13 -0400

    0.1 version

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 /* Author:
-
+Rick Chen
 */
-
+var kkeys = [];
+var pin = "13";
+$(document).keydown(function(e) {
+	kkeys.push(e.keyCode);
+	if (kkeys.toString().indexOf(pin) >= 0)
+	{
+		kkeys = []; 
+		if($('._jGoodfeeling_container').length === 0){
+	   		$('body').jGoodfeeling();
+		}   
+	}
+}); 
 
 
 
