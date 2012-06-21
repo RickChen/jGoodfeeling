848f59d1ca599ba820983518ff0b91ca17908afb
Rick Chen <rick@rickchendesign.com>
2012-06-21 13:17:08 -0400

    fix closing

--- js/jquery.jGoodfeeling.js
+++ js/jquery.jGoodfeeling.js
@@ -5,38 +5,38 @@
  * to offer multiple easing options
  *
  * TERMS OF USE - jQuery Easing
- *
- * Open source under the BSD License.
- *
+ * 
+ * Open source under the BSD License. 
+ * 
  * Copyright Â© 2008 George McGinley Smith
  * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, 
  * are permitted provided that the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list of
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list of 
  * conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright notice, this list
- * of conditions and the following disclaimer in the documentation and/or other materials
+ * Redistributions in binary form must reproduce the above copyright notice, this list 
+ * of conditions and the following disclaimer in the documentation and/or other materials 
  * provided with the distribution.
- *
- * Neither the name of the author nor the names of contributors may be used to endorse
+ * 
+ * Neither the name of the author nor the names of contributors may be used to endorse 
  * or promote products derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
  * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+ * OF THE POSSIBILITY OF SUCH DAMAGE. 
  *
  */
 
 // t: current time, b: begInnIng value, c: change In value, d: duration
-jQuery.easing['jswing'] = jQuery.easing['swing'];
+jQuery.easing['jswing']           = jQuery.easing['swing'];
 
 jQuery.extend( jQuery.easing,
     {
@@ -59,7 +59,7 @@ easeInCubic: function (x, t, b, c, d) {
 return c*(t/=d)*t*t + b;
 },
 easeOutCubic: function (x, t, b, c, d) {
-                return c*((t = t/d-1)*t*t + 1) + b;
+                return c*((t                      = t/d-1)*t*t + 1) + b;
               },
 easeInOutCubic: function (x, t, b, c, d) {
                   if ((t/=d/2) < 1) return c/2*t*t*t + b;
@@ -69,7 +69,7 @@ easeInQuart: function (x, t, b, c, d) {
                return c*(t/=d)*t*t*t + b;
              },
 easeOutQuart: function (x, t, b, c, d) {
-                return -c * ((t = t/d-1)*t*t*t - 1) + b;
+                return -c * ((t                   = t/d-1)*t*t*t - 1) + b;
               },
 easeInOutQuart: function (x, t, b, c, d) {
                   if ((t/=d/2) < 1) return c/2*t*t*t*t + b;
@@ -79,7 +79,7 @@ easeInQuint: function (x, t, b, c, d) {
                return c*(t/=d)*t*t*t*t + b;
              },
 easeOutQuint: function (x, t, b, c, d) {
-                return c*((t = t/d-1)*t*t*t*t + 1) + b;
+                return c*((t                      = t/d-1)*t*t*t*t + 1) + b;
               },
 easeInOutQuint: function (x, t, b, c, d) {
                   if ((t/=d/2) < 1) return c/2*t*t*t*t*t + b;
@@ -110,31 +110,31 @@ easeInCirc: function (x, t, b, c, d) {
               return -c * (Math.sqrt(1 - (t/=d)*t) - 1) + b;
             },
 easeOutCirc: function (x, t, b, c, d) {
-               return c * Math.sqrt(1 - (t = t/d-1)*t) + b;
+               return c * Math.sqrt(1 - (t       = t/d-1)*t) + b;
              },
 easeInOutCirc: function (x, t, b, c, d) {
                  if ((t/=d/2) < 1) return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;
                  return c/2 * (Math.sqrt(1 - (t-=2)*t) + 1) + b;
                },
 easeInElastic: function (x, t, b, c, d) {
-                 var s = 1.70158;var p=0;var a=c;
-                 if (t==0) return b; if ((t/=d)==1) return b+c; if (!p) p=d*.3;
-                 if (a < Math.abs(c)) { a = c; var s=p/4; }
-                 else var s = p/(2*Math.PI) * Math.asin (c/a);
+                 var s                             = 1.70158;var p=0;var a=c;
+                 if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+                 if (a < Math.abs(c)) { a          = c; var s=p/4; }
+                 else var s                        = p/(2*Math.PI) * Math.asin (c/a);
                  return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
                },
 easeOutElastic: function (x, t, b, c, d) {
-                  var s = 1.70158;var p=0;var a=c;
-                  if (t==0) return b; if ((t/=d)==1) return b+c; if (!p) p=d*.3;
-                  if (a < Math.abs(c)) { a = c; var s=p/4; }
-                  else var s = p/(2*Math.PI) * Math.asin (c/a);
+                  var s                             = 1.70158;var p=0;var a=c;
+                  if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+                  if (a < Math.abs(c)) { a          = c; var s=p/4; }
+                  else var s                        = p/(2*Math.PI) * Math.asin (c/a);
                   return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
                 },
 easeInOutElastic: function (x, t, b, c, d) {
-                    var s = 1.70158;var p=0;var a=c;
-                    if (t==0) return b; if ((t/=d/2)==2) return b+c; if (!p) p=d*(.3*1.5);
-                    if (a < Math.abs(c)) { a = c; var s=p/4; }
-                    else var s = p/(2*Math.PI) * Math.asin (c/a);
+                    var s                             = 1.70158;var p=0;var a=c;
+                    if (t==0) return b;  if ((t/=d/2)==2) return b+c;  if (!p) p=d*(.3*1.5);
+                    if (a < Math.abs(c)) { a          = c; var s=p/4; }
+                    else var s                        = p/(2*Math.PI) * Math.asin (c/a);
                     if (t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
                     return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )*.5 + c + b;
                   },
@@ -144,10 +144,10 @@ easeInBack: function (x, t, b, c, d, s) {
             },
 easeOutBack: function (x, t, b, c, d, s) {
                if (s == undefined) s = 1.70158;
-               return c*((t = t/d-1)*t*((s+1)*t + s) + 1) + b;
+               return c*((t                      = t/d-1)*t*((s+1)*t + s) + 1) + b;
              },
 easeInOutBack: function (x, t, b, c, d, s) {
-                 if (s == undefined) s = 1.70158;
+                 if (s == undefined) s = 1.70158; 
                  if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
                  return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
                },
@@ -174,33 +174,33 @@ easeInOutBounce: function (x, t, b, c, d) {
 /*
  *
  * TERMS OF USE - EASING EQUATIONS
- *
- * Open source under the BSD License.
- *
+ * 
+ * Open source under the BSD License. 
+ * 
  * Copyright Â© 2001 Robert Penner
  * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, 
  * are permitted provided that the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list of
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list of 
  * conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright notice, this list
- * of conditions and the following disclaimer in the documentation and/or other materials
+ * Redistributions in binary form must reproduce the above copyright notice, this list 
+ * of conditions and the following disclaimer in the documentation and/or other materials 
  * provided with the distribution.
- *
- * Neither the name of the author nor the names of contributors may be used to endorse
+ * 
+ * Neither the name of the author nor the names of contributors may be used to endorse 
  * or promote products derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
  * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+ * OF THE POSSIBILITY OF SUCH DAMAGE. 
  *
  */
 
@@ -214,7 +214,7 @@ easeInOutBounce: function (x, t, b, c, d) {
  if (data) {
  if ($.fn.jGoodfeeling.defaultSettings[option] !== undefined) {
  if (settings !== undefined) {
- //if you need to make any specific changes to the DOM make them here
+ //if you need to make any specific changes to the DOM make them here 
  data.settings[option] = settings;
  return true;
  } else return data.settings[option];
@@ -233,7 +233,7 @@ easeInOutBounce: function (x, t, b, c, d) {
 
    var goodFeelingContainer = goodfeeling.generate();
 
-   $('html, body').animate({scrollTop: $("body").offset().top}, 250);
+   $('html, body').animate({scrollTop: $("body").offset().top}, 250); 
 
    $('body').append(goodFeelingContainer);
 
@@ -243,7 +243,7 @@ easeInOutBounce: function (x, t, b, c, d) {
     } else {
     return
     }
-    })();
+    })();             
 
    goodfeeling.attachClose();
 
@@ -251,7 +251,7 @@ easeInOutBounce: function (x, t, b, c, d) {
  });
  }
 
- $.fn.jGoodfeeling.defaultSettings = {
+ $.fn.jGoodfeeling.defaultSettings = { 
 fonts:null,
       prefix: "_jGoodfeeling",
       containerHeight: getHeight() * 1.1,
@@ -264,16 +264,16 @@ fonts:null,
       sliceDelay: 500,
       sliceZ: 9999,
       sliceOpacity: 1,
-      sliceInitOpacity: 0,
+      sliceInitOpacity: 0,    
       modalWidth: 700,
       modalHeight: 360,
-      modalCSS: {
+      modalCSS: {     
         'background' : 'white',
         'background' : 'rgba(255, 255, 255, 1)',
         '-webkit-box-shadow': '0px 0px 20px rgba(0, 0, 0, 0.5)',
         '-moz-box-shadow': '0px 0px 20px rgba(0, 0, 0, 0.5)',
         '-moz-border-radius': '5px',
-        '-webkit-border-radius': '5px'
+        '-webkit-border-radius': '5px'    
       },
 modalHeader :'Default Header',
              modalHeaderCSS: {
@@ -285,14 +285,14 @@ modalBody: 'Default Body Text',
            modalBodyCSS: {
              'color' : '#CB2027',
              'font-size': '2em',
-             'text-align':'center'
+             'text-align':'center'         
            },
 modalZ: 99999,
         modalAnimationTime: 4000,
         colorScheme : ['#C84281', '#F0624F','#84399F','#A2DD49','#E390B7','#F7A79C', '#BC86CF', '#CBEE96'],
         colorScheme : ['#CB2029', '#CF6C21', '#A31A6D', '#28A91B', '#E57B80', '#E7AA7C', '#D170AB', '#7BD472'],
         colorScheme : ['#78E700','#00B454','#D8F800','#DE0052','#9BF33D','#36DA82','#E3FC3F','#EE3C7E'],
-        colorScheme : ['#eee', '#ddd', '#ccc', '#bbb', '#aaa', '#999','#888','#777', '#666','#555','#444','#333']
+        colorScheme : ['#eee', '#ddd', '#ccc', '#bbb', '#aaa', '#999','#888','#777', '#666','#555','#444','#333']     
  };
 
  function Goodfeeling(settings) {
@@ -326,7 +326,7 @@ generate: function () {
             if ($this.goodfeeling) return $this.goodfeeling;
 
             $this.goodfeelingContainer = $('<div class="' + $this.settings.prefix + '_container"></div>');
-            $this.goodmodalContainer = $('<div class="' + $this.settings.prefix + '_modal_container"></div>');
+            $this.goodmodalContainer = $('<div class="' + $this.settings.prefix + '_modal"></div>');
             $this.goodfeelingContainer.data('doneLoading', false);
 
             var containerCSS = {
@@ -345,7 +345,7 @@ generate: function () {
               'height': getHeight(),
               'left': 0,
               'top': 0,
-              'z-index': 2
+              'z-index': 1
             }
             $this.goodfeelingContainer.css(containerCSS);
             $this.goodmodalContainer.css(modalcontainerCSS);
@@ -364,7 +364,7 @@ doneAnimating: function () {
 
 animationInit: function () {
                  var $this = this,
-                 linearDelay = $this.settings.linearDelay;
+                 linearDelay = $this.settings.linearDelay; 
 
                  $this.attachModal();
                  if ($this.settings.animationType === "linear") {
@@ -377,9 +377,9 @@ animationInit: function () {
                          }, $this.settings.animationTime);
                        });
 
-                 } else if ($this.settings.animationType === "diagonal") {
+                 } else if ($this.settings.animationType === "diagonal") {                
                    var index = 0;
-                   setInterval(function () {
+                   setInterval(function () {                   
                        var levelSlices = $('.' + $this.settings.prefix + '_level_' + index),
                        sliceOpacity = $this.settings.sliceOpacity,
                        animationTime = $this.settings.animationTime;
@@ -397,7 +397,7 @@ animationInit: function () {
 
                },
 
-attachModal: function () {
+attachModal: function () {          
                var $this = this,
 
                $modal = $('<div class="' + $this.settings.prefix + '_modal_container"></div>'),
@@ -408,22 +408,22 @@ attachModal: function () {
                    'top': '-100%',
                    'padding': '1em',
                    'font-size': '1em',
-                   'overflow': 'hidden',
+                   'overflow': 'hidden',                                   
                    'width': $this.settings.modalWidth,
                    'height': $this.settings.modalHeight,
                    'margin-left': '-' + $this.settings.modalWidth / 2 + 'px',
                    'margin-top': '-' + $this.settings.modalHeight / 2 + 'px',
-                   'z-index': $this.settings.modalZ
-                   }, $this.settings.modalCSS),
+                   'z-index': $this.settings.modalZ  
+                   }, $this.settings.modalCSS), 
 
                modalHeaderCSS = $.extend({},$this.settings.modalHeaderCSS),
-               modalBodyCSS = $.extend({},$this.settings.modalBodyCSS),
+               modalBodyCSS = $.extend({},$this.settings.modalBodyCSS), 
                $modalContent = $('<div class="' + $this.settings.prefix + '_modal"></div>'),
                $modalHeader = $('<div class="' + $this.settings.prefix + '_modal_header">' + $this.settings.modalHeader + '</div>'),
-               $modalBody = $('<div class="' + $this.settings.prefix + '_modal_header">' + $this.settings.modalBody + '</div>');
+               $modalBody = $('<div class="' + $this.settings.prefix + '_modal_header">' + $this.settings.modalBody + '</div>'); 
 
                if($this.settings.fonts){
-                 $('head').append($this.settings.fonts);
+                 $('head').append($this.settings.fonts); 
                }
 
                $modalHeader.css(modalHeaderCSS);
@@ -443,8 +443,9 @@ $this.globals.doneAnimating = true;
 },
 attachClose: function () {
                var $this = this;
-               $('.' + $this.settings.prefix + '_container').click(function () {
-                   if($this.doneAnimating()) {
+               $(window).click(function (e) {
+                   var target = $(e.target);
+                   if($this.doneAnimating() && target.hasClass($this.settings.prefix + '_modal')) {
                    $('.' + $this.settings.prefix + '_container, .' + $this.settings.prefix + '_modal_container').fadeOut(2000, function () {
                      $(this).remove();
                      });
@@ -494,6 +495,6 @@ attachSlices: function () {
                 }
 
                 $container.data('doneLoading', true);
-              }
+              } 
 }
 })(jQuery);
